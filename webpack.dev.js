d9f16e91c242bcd68d612adcd2b0398a1c216922
Mampionona33 <ramamps33@gmail.com>
2022-09-01 10:57:10 +0300

    adding historyfalback in webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,7 @@ module.exports = merge(common, {
     static: {
       directory: path.join(__dirname, 'public'),
     },
+    historyApiFallback: true,
     hot: true,
     compress: true,
     port: 8000,
